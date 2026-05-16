3680d3b5f4fdc2fcb7c3fde0c46f6afd6e4eef90
허윤하 <yoonha@heoyoonha-ui-MacBookAir.local>
2026-05-16 18:09:03 +0900

    Optimistically update favorites

--- hooks/useToogleFavorite.ts
+++ hooks/useToogleFavorite.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toggleFavoriteApi } from "../apis/favorite";
 import { useAuthStore } from "../store/authStore";
+import { Product } from "../apis/types";
 
 export const useToggleFavorite = () => {
   const { accessToken } = useAuthStore();
@@ -10,8 +11,37 @@ export const useToggleFavorite = () => {
     mutationFn: (productId: string) =>
       toggleFavoriteApi(accessToken!, productId),
 
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["products"] });
+    onMutate: async (productId: string) => {
+      await queryClient.cancelQueries({
+        queryKey: ["products"],
+      });
+
+      const previousProducts = queryClient.getQueryData<Product[]>([
+        "products",
+      ]);
+
+      queryClient.setQueryData<Product[]>(["products"], (old) => {
+        if (!old) return old;
+
+        return old.map((product) =>
+          product.id === productId
+            ? {
+                ...product,
+                isFavorite: !product.isFavorite,
+              }
+            : product,
+        );
+      });
+
+      return {
+        previousProducts,
+      };
+    },
+
+    onError: (_error, _productId, context) => {
+      if (context?.previousProducts) {
+        queryClient.setQueryData(["products"], context.previousProducts);
+      }
     },
   });
 };
